f1178af6c84b26459ef8913a8d6415f26eadcf06
TeleKxd <piskulin.i@mail.ru>
2024-10-27 23:03:34 +0300

    Task5

---
+++ Tyuiu.PiskulinIY.Sprint3.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+using Tyuiu.PiskulinIY.Sprint3.Task5.V1.Lib;
+namespace Tyuiu.PiskulinIY.Sprint3.Task5.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 5;
+            int startValue1 = 1;
+            int startValue2 = 1;
+            int stopValue1 = 3;
+            int stopValue2 = 10;
+
+            double res = ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2);
+
+            double wait = 7.21;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PiskulinIY.Sprint3.Task5.V1/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.PiskulinIY.Sprint3.Task5.V1.Lib;
+namespace Tyuiu.PiskulinIY.Sprint3.Task5.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Организация ввода\\вывода в консольных приложениях                *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Пискулин Игорь Юрьевич | ИСТНб-24-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле x*y*(5-x) и печатает его на экране.      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            int x = 5;
+            int startValue1 = 1;
+            int startValue2 = 1;
+            int stopValue1 = 3;
+            int stopValue2 = 10;
+
+            Console.WriteLine("Переменная X =" + x);
+            Console.WriteLine("Старт шага первой суммы ряда =" + startValue1);
+            Console.WriteLine("Конец шага первой суммы ряда =" + stopValue1);
+            Console.WriteLine("Старт шага второй суммы ряда =" + startValue2);
+            Console.WriteLine("Конец шага второй суммы ряда =" + stopValue2);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+
+            Console.ReadKey();
+        }
+    }
+}
